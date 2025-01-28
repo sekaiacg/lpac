9878dcb3f4bf0ca21cc69e97b6d90d881d7d97c1
sekaiacg <sekaiacg@gmail.com>
2025-01-28 20:51:11 +0800

    libeuicc: Provide base64 encoded nickname
    Support for non-UTF-8 encoded nickname
    
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- src/applet/profile/nickname.c
+++ src/applet/profile/nickname.c
@@ -1,6 +1,7 @@
 #include "nickname.h"
 #include "main.h"
 
+#include <euicc/base64.h>
 #include <euicc/es10c.h>
 
 #include <stdio.h>
@@ -11,22 +12,39 @@
 static int applet_main(int argc, char **argv) {
     int ret;
     const char *iccid;
-    const char *new_name;
+    const char *new_name = NULL;
+    char *new_name_decode = NULL;
+    int name_bytes_len = 0;
 
     if (argc < 2) {
-        printf("Usage: %s [iccid] [new_name]\n", argv[0]);
+        printf("Usage: %s [iccid] [new_name] [name_bytes_size]\n", argv[0]);
         printf("\t[new_name]: optional\n");
+        printf("\t[name_bytes_len]: optional, If provided, the nickname will be decoded using base64.\n");
         return -1;
     }
 
     iccid = argv[1];
-    if (argc > 2) {
+    if (argc > 3) {
+        new_name_decode = (char *)malloc((size_t)euicc_base64_decode_len(argv[2]));
+        if (new_name_decode) {
+            int len = euicc_base64_decode((unsigned char *)new_name_decode, argv[2]);
+            if (len > 0 && len == atoi(argv[3])) {
+                new_name = (const char *)new_name_decode;
+                name_bytes_len = len;
+            }
+        }
+        if (!new_name) {
+            jprint_error("es10c_set_nickname", "base64 decode new_name failed");
+            return -1;
+        }
+    } else if (argc > 2) {
         new_name = argv[2];
+        name_bytes_len = (int)strlen(new_name);
     } else {
         new_name = "";
     }
 
-    if ((ret = es10c_set_nickname(&euicc_ctx, iccid, new_name))) {
+    if ((ret = es10c_set_nickname(&euicc_ctx, iccid, new_name, name_bytes_len))) {
         const char *reason;
         switch (ret) {
         case 1:
@@ -39,6 +57,8 @@ static int applet_main(int argc, char **argv) {
         jprint_error("es10c_set_nickname", reason);
         return -1;
     }
+
+    free(new_name_decode);
 
     jprint_success(NULL);
 
