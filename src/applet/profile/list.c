a3c647e14fa56cc2f5662e33abc1f8ed61e1fd53
sekaiacg <sekaiacg@gmail.com>
2025-01-28 20:51:11 +0800

    libeuicc: Provide base64 encoded nickname
    Support for non-UTF-8 encoded nickname
    
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- src/applet/profile/list.c
+++ src/applet/profile/list.c
@@ -1,6 +1,7 @@
 #include "list.h"
 #include "main.h"
 
+#include <euicc/base64.h>
 #include <euicc/es10c.h>
 #include <euicc/tostr.h>
 #include <lpac/utils.h>
@@ -9,6 +10,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+char *handleProfileNicknameBase64(__attribute__((unused)) cJSON *jprofile, struct es10c_profile_info_list *profile) {
+    const char *profileNickname = profile->profileNickname;
+    const int profileNicknameLength = profile->profileNicknameLength;
+    char *b64Nickname = NULL;
+    if (profileNickname) {
+        size_t size = (size_t)euicc_base64_encode_len(profileNicknameLength);
+        b64Nickname = (char *)malloc(size);
+        if (b64Nickname) {
+            memset(b64Nickname, 0, size);
+            euicc_base64_encode(b64Nickname, (const unsigned char *)profileNickname, profileNicknameLength);
+        }
+    }
+    return b64Nickname;
+}
 
 static int applet_main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv) {
     _cleanup_es10c_profile_info_list_ struct es10c_profile_info_list *profiles;
@@ -25,12 +41,14 @@ static int applet_main(__attribute__((unused)) int argc, __attribute__((unused))
 
     while (rptr) {
         cJSON *jprofile = NULL;
+        char *b64Nickname = handleProfileNicknameBase64(jprofile, rptr);
 
         jprofile = cJSON_CreateObject();
         cJSON_AddStringOrNullToObject(jprofile, "iccid", rptr->iccid);
         cJSON_AddStringOrNullToObject(jprofile, "isdpAid", rptr->isdpAid);
         cJSON_AddStringOrNullToObject(jprofile, "profileState", euicc_profilestate2str(rptr->profileState));
         cJSON_AddStringOrNullToObject(jprofile, "profileNickname", rptr->profileNickname);
+        cJSON_AddStringOrNullToObject(jprofile, "profileNicknameB64", b64Nickname);
         cJSON_AddStringOrNullToObject(jprofile, "serviceProviderName", rptr->serviceProviderName);
         cJSON_AddStringOrNullToObject(jprofile, "profileName", rptr->profileName);
         cJSON_AddStringOrNullToObject(jprofile, "iconType", euicc_icontype2str(rptr->iconType));
@@ -38,6 +56,7 @@ static int applet_main(__attribute__((unused)) int argc, __attribute__((unused))
         cJSON_AddStringOrNullToObject(jprofile, "profileClass", euicc_profileclass2str(rptr->profileClass));
         cJSON_AddItemToArray(jdata, jprofile);
 
+        free(b64Nickname);
         rptr = rptr->next;
     }
 
